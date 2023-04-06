145ab421b6ee708e947f8dd82879dd2d260792a3
meskt <56514258+meskt@users.noreply.github.com>
2023-04-06 16:18:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,8 +6,6 @@ from scipy.stats import chi2
 chat_id = 1841341924 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    return (-min(-x) - 1/2)*2/38**2, (-np.log(alpha)/len(x) - min(-x) - 1/2)*2/38**2
+    return (-min(-x) - 0.5) * 2 / 38 ** 2, \
+        (-np.log(alpha) / len(x) - min(-x) - 0.5) * 2 / 38 ** 2
